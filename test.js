0a01c37a6bf842613d97291ec65bf95202e39850
Sakshi-180620 <spatidar1806@gmail.com>
2026-08-10 10:12:36 +0530

    Add files via upload

--- test.js
+++ test.js
@@ -1,50 +1,12 @@
-const fs = require("fs");
+let result = checkFile("script.js");
 
-function checkFile(fileName) {
-
-    if (fs.existsSync(fileName)) {
-        console.log(fileName + " : File Exists");
-        return true;
-    } else {
-        console.error(fileName + " : File Not Found");
-        return false;
-    }
-}
-
-
-// 10 Test Cases
-
-let result1 = checkFile("indexx.html");
-let result2 = checkFile("script.js");
-let result3 = checkFile("student.json");
-let result4 = checkFile("style.css");
-let result5 = checkFile("about.html");
-let result6 = checkFile("contact.html");
-let result7 = checkFile("README.md");
-let result8 = checkFile("login.html");
-let result9 = checkFile("register.html");
-let result10 = checkFile("data.json");
-
-
-// Check Test Results
-
-if (
-    !result1 ||
-    !result2 ||
-    !result3 ||
-    !result4 ||
-    !result5 ||
-    !result6 ||
-    !result7 ||
-    !result8 ||
-    !result9 ||
-    !result10
-) {
-    console.error("TEST FAILED");
+if (!result) {
+    console.error("TEST CASE 2 FAILED");
     process.exit(1);
 } else {
-    console.log("ALL 10 TEST CASES PASSED");
+    console.log("TEST CASE 2 PASSED");
     process.exit(0);
 }
+
 
 
